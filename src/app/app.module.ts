16c81f158a93138ceea4d5e5cc6c025bdee29137
thapaRoyal <thaparoyal17@gmail.com>
2023-03-11 13:25:39 +0545

    registration form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,13 @@ import { NgConfirmModule } from 'ng-confirm-box';
 import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatIconModule} from '@angular/material/icon'
 import {MatButtonModule} from '@angular/material/button'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input'
+import {MatRadioModule} from '@angular/material/radio'
+import {MatSelectModule} from '@angular/material/select'; 
+import {MatDatepickerModule} from '@angular/material/datepicker'; 
+import {MatNativeDateModule} from '@angular/material/core'; 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,7 +35,7 @@ import {MatButtonModule} from '@angular/material/button'
     NgConfirmModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,MatFormFieldModule,MatInputModule,MatRadioModule,MatSelectModule,MatDatepickerModule, MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-registration/create-registration.component.ts
+++ src/app/create-registration/create-registration.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-registration.component.scss']
 })
 export class CreateRegistrationComponent {
+  public packages:string[] =  ['Monthly', "Quaterly", "Yearly"];
+  public genders:string[] = ["Male", "Female"];
+  public importantList:string[] = [
+    "Toxic fat reduction",
+    "Energy and Endurance",
+    "Building lean muscle",
+    "Healthier Digestive Function",
+    "Fitness"
+  ]
 
 }

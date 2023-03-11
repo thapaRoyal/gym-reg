0fd87182961d1e81612a5a166f4735773290e8c6
thapaRoyal <thaparoyal17@gmail.com>
2023-03-11 12:36:00 +0545

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
 import { NgToastModule } from 'ng-angular-popup';
 import { NgConfirmModule } from 'ng-confirm-box';
 
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule} from '@angular/material/icon'
+import {MatButtonModule} from '@angular/material/button'
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +25,10 @@ import { NgConfirmModule } from 'ng-confirm-box';
     AppRoutingModule,
     BrowserAnimationsModule,
     NgToastModule,
-    NgConfirmModule
+    NgConfirmModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

773947633bbe46878b813fe05d8c16e5f99747d2
thapaRoyal <thaparoyal17@gmail.com>
2023-03-11 12:39:09 +0545

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { RegistrationListComponent } from './registration-list/registration-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', redirectTo:'register', pathMatch:'full'},
+  {path:'register',  component:CreateRegistrationComponent},
+  {path:'list', component:RegistrationListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
